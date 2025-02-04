33e566c65e3630ee53323216c92741627c583104
myulbarsov <myulbarsov@stu.naperville203.org>
2025-02-03 19:48:48 -0600

    Create updateAnnouncement ts files
    establish connection with socket, continue implementing updateAnnouncement.

---
+++ static/ts/updateAnnouncementConnect.ts
@@ -0,0 +1,20 @@
+/// <reference path="./socket-io-client.d.ts"/>
+
+var adminSocket = window.io('/admin');
+
+adminSocket.on("update", (data) => {
+    console.log("update received")
+
+    console.log(data)
+
+    window.location.reload();
+    
+
+});
+
+function updateAnnouncement() {
+    console.log("update called")
+    adminSocket.emit("updateMain", {
+        type: "update",
+    });
+}
